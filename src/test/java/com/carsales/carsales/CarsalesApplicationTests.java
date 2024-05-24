3d1c3f4efaf4eacdeabddb79812607ef8cb256b2
Sujan <mail.thapasujan@gmail.com>
2024-05-24 10:33:52 +1000

    initialised empty hello world app in spring boot

---
+++ src/test/java/com/carsales/carsales/CarsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsales.carsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
